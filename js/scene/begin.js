03c6faf9f8510f474b1b54d7e1e4cad411de6b21
zhangboheng <zhangboheng827@gmail.com>
2024-03-01 00:06:39 +0800

    增加人物左右移动

--- js/scene/begin.js
+++ js/scene/begin.js
@@ -1,6 +1,5 @@
 import {
-  createBackButton,
-  drawRoundedRectWithTail
+  createBackButton
 } from '../../utils/button';
 let systemInfo = wx.getSystemInfoSync();
 let menuButtonInfo = wx.getMenuButtonBoundingClientRect();
@@ -15,10 +14,50 @@ export default class Instruction {
     // 绘制背景
     this.backgroundImage = new Image();
     this.backgroundImage.src = 'image/thumbnail.jpg';
+    // 绘制游戏区域背景
+    this.gameBackground = new Image();
+    this.gameBackground.src = 'image/background.jpg';
+    // 绘制陆地
+    this.yardImage = new Image();
+    this.yardImage.src = 'image/yard.jpg';
     // 创建返回按钮
     this.backButton = createBackButton(this.context, 10, menuButtonInfo.top, 'image/reply.png', () => {
       this.game.switchScene(new this.game.startup(this.game));
     });
+    // 绘制人物
+    this.character = {
+      width: 20,
+      height: 35,
+      x: 35,
+      y: menuButtonInfo.bottom + this.canvas.height * 0.5 + 6 - 35,
+      speed: systemInfo.screenWidth * 0.005, // 人物每次移动的距离
+      rightFrames: [], // 存储向右帧图片的数组
+      leftFrames: [], // 存储向左帧图片的数组
+      jumpFrames: [], // 存储弹跳图片的数组
+      shotFrames: [], // 存储发射图片的数组
+      currentRightFrameIndex: 0,
+      currentLeftFrameIndex: 0,
+      theLastAction: '', // 按钮停下后最后一个动作
+    };
+    // 向右移动时候图片集锦
+    const framePathsRight = ['image/right1.png', 'image/right2.png', 'image/right3.png'];
+    for (const path of framePathsRight) {
+      const frame = new Image();
+      frame.src = path;
+      this.character.rightFrames.push(frame);
+    }
+    // 向左移动时候图片集锦
+    const framePathsLeft = ['image/left1.png', 'image/left2.png', 'image/left3.png']
+    for (const path of framePathsLeft) {
+      const frame = new Image();
+      frame.src = path;
+      this.character.leftFrames.push(frame);
+    }
+    // 添加触摸事件监听
+    wx.onTouchStart(this.touchStartHandler.bind(this));
+    wx.onTouchEnd(this.touchEndHandler.bind(this));
+    this.pressStartTime = 0; // 记录长按开始时间
+    this.pressingDirection = null; // 记录当前长按的方向键
   }
   // 绘制背景
   drawBackground() {
@@ -44,10 +83,40 @@ export default class Instruction {
     const rectHeight = this.canvas.height * 0.6;
     this.context.fillStyle = '#000000'; // 黑色
     this.context.fillRect(13, menuButtonInfo.bottom + 10, this.canvas.width - 26, rectHeight);
-    this.context.strokeStyle = 'white';
+    this.context.strokeStyle = '#ffffff99';
     this.context.lineWidth = 3;
     this.context.strokeRect(13, menuButtonInfo.bottom + 10, this.canvas.width - 26, rectHeight);
     this.context.restore();
+  }
+  // 绘制游戏背景
+  drawGameBackground() {
+    if(this.gameBackground.complete){
+      this.context.drawImage(this.gameBackground, 16, menuButtonInfo.bottom + 13, this.canvas.width - 32, this.canvas.height * 0.6 - 6)
+    }
+  }
+  // 绘制陆地
+  drawYard() {
+    if(this.yardImage.complete){
+      this.context.drawImage(this.yardImage, 16, menuButtonInfo.bottom + this.canvas.height * 0.5 + 6, this.canvas.width - 32, this.canvas.height * 0.1)
+    }
+  }
+  // 绘制人物
+  drawCharacter() {
+    if(this.character.theLastAction == 'right'){
+      this.context.drawImage(this.character.rightFrames[this.character.currentRightFrameIndex], this.character.x, this.character.y, this.character.width, this.character.height);
+    }else if(this.character.theLastAction == 'left'){
+      this.context.drawImage(this.character.leftFrames[this.character.currentLeftFrameIndex], this.character.x, this.character.y, this.character.width, this.character.height);
+    }else{
+      this.context.drawImage(this.character.rightFrames[this.character.currentRightFrameIndex], this.character.x, this.character.y, this.character.width, this.character.height);
+    }
+  }
+  // 更新绘制人物
+  updateCharacter() {
+    if(this.pressingDirection == 'right'){
+      this.moveRightCharacter();
+    }else if(this.pressingDirection == 'left'){
+      this.moveLeftCharacter();
+    }
   }
   // 绘制手柄
   drawJoystick() {
@@ -149,69 +218,149 @@ export default class Instruction {
     // 绘制游戏活动区域
     this.drawActionZone();
     // 绘制移动按钮所在区域
-    this.drawJoystick()
+    this.drawJoystick();
+    // 绘制游戏背景
+    this.drawGameBackground()
+    // 绘制陆地
+    this.drawYard();
+    // 绘制人物
+    this.drawCharacter()
   }
   update() {
-
+    this.updateCharacter();
   }
-  touchHandler(e) {
+  // 通用返回按钮作用区
+  touchHandler(e){
     const touch = e.touches[0];
     const canvasRect = this.canvas.getBoundingClientRect();
     const touchX = touch.clientX - canvasRect.left;
     const touchY = touch.clientY - canvasRect.top;
     const btn = this.backButton;
-    const joystickSize = Math.min(this.canvas.width, this.canvas.height) * 0.2;
-    const joystickY = this.canvas.height * 0.85;
-    // 绘制方向键（矩形）
-    const arrowSize = joystickSize * 0.5;
-    const padSize = joystickSize * 0.6;
-    const buttonSize = padSize * 1;
     // 点击返回按钮事件
     if (touchX >= btn.x && touchX <= btn.x + btn.width &&
       touchY >= btn.y && touchY <= btn.y + btn.height) {
       btn.onClick();
       return
     }
+  }
+  touchStartHandler(e) {
+    const touch = e.touches[0];
+    const canvasRect = this.canvas.getBoundingClientRect();
+    const touchX = touch.clientX - canvasRect.left;
+    const touchY = touch.clientY - canvasRect.top;
+    const joystickSize = Math.min(this.canvas.width, this.canvas.height) * 0.2;
+    const joystickY = this.canvas.height * 0.85;
+    // 绘制方向键（矩形）
+    const arrowSize = joystickSize * 0.5;
+    const padSize = joystickSize * 0.6;
+    const buttonSize = padSize * 1;
     // 判断点击位置是否在方向键上
     if (
       touchX >= 23 + arrowSize && touchX <= 23 + arrowSize + buttonSize &&
       touchY >= joystickY - padSize / 2 - buttonSize && touchY <= joystickY - padSize / 2
     ) {
       // 上方向键
-      console.log('1');
+      this.pressingDirection = 'up';
+      this.pressStartTime = Date.now();
+      this.character.theLastAction = 'up';
     } else if (
       touchX >= 23 + arrowSize && touchX <= 23 + arrowSize + buttonSize &&
       touchY >= joystickY + padSize / 2 && touchY <= joystickY + padSize / 2 + buttonSize
     ) {
       // 下方向键
-      console.log('2');
+      this.pressingDirection = 'down';
+      this.pressStartTime = Date.now();
+      this.character.theLastAction = 'down';
     } else if (
       touchX >= 13 && touchX <= 13 + buttonSize &&
       touchY >= joystickY - buttonSize / 2 && touchY <= joystickY + buttonSize / 2
     ) {
       // 左方向键
-      console.log('3');
+      this.pressingDirection = 'left';
+      this.pressStartTime = Date.now();
+      this.character.theLastAction = 'left';
+      this.moveLeftCharacter();
     } else if (
       touchX >= 33 + arrowSize * 2 && touchX <= 33 + arrowSize * 2 + buttonSize &&
       touchY >= joystickY - buttonSize / 2 && touchY <= joystickY + buttonSize / 2
     ) {
       // 右方向键
-      console.log('4');
+      this.pressingDirection = 'right';
+      this.pressStartTime = Date.now();
+      this.character.theLastAction = 'right';
+      this.moveRightCharacter();
     } else if (
       touchX >= menuButtonInfo.right - buttonSize * 2.5 && touchX <= menuButtonInfo.right - buttonSize * 2.5 + buttonSize &&
       touchY >= joystickY - padSize / 2 + buttonSize / 2 && touchY <= joystickY - padSize / 2 + buttonSize * 3/2) {
       // 发射
-      console.log('shot');
+      this.character.theLastAction = 'shot';
     } else if (
       touchX >= menuButtonInfo.right - buttonSize && touchX <= menuButtonInfo.right &&
       touchY >= joystickY - padSize / 2 - buttonSize / 2 && touchY <= joystickY - padSize / 2 + buttonSize / 2) {
       // 跳跃
-      console.log('jump');
+      this.character.theLastAction = 'jump';
     }
+  }
+  touchEndHandler(e) {
+    if (this.pressingDirection == 'right') {
+      this.character.currentRightFrameIndex = 0;
+    }else if(this.pressingDirection == 'left'){
+      this.character.currentLeftFrameIndex = 0;
+    }
+    this.pressingDirection = null; // 清空按键方向
+    this.pressStartTime = 0; // 清空长按开始时间
+  }
+  // 向右移动动作
+  moveRightCharacter(){
+    this.character.x += this.character.speed;
+    if (this.character.x >= this.canvas.width - 33){
+      this.character.x = this.canvas.width - 33
+    }
+    this.character.currentRightFrameIndex = (this.character.currentRightFrameIndex + 1) % this.character.rightFrames.length;
+  }
+  // 向左移动动作
+  moveLeftCharacter(){
+    this.character.x -= this.character.speed;
+    if (this.character.x <= 13){
+      this.character.x = 13
+    }
+    this.character.currentLeftFrameIndex = (this.character.currentLeftFrameIndex + 1) % this.character.leftFrames.length;
   }
   // 页面销毁机制
   destroy() {
-    // 清理资源，如图片
+    // 移除触摸事件监听器
+    wx.offTouchStart(this.touchStartHandler.bind(this));
+    wx.offTouchEnd(this.touchEndHandler.bind(this));
+    // 清理加载图片
     this.backButton.image.src = '';
+    this.backgroundImage.src = '';
+    this.gameBackground.src = '';
+    this.yardImage.src = '';
+    // 清理人物图片
+    this.destroyCharacterFrames();
+  }
+  // 返回其他层后销毁加载图片，避免内存泄漏
+  destroyCharacterFrames() {
+    for (let i = 0; i < this.character.rightFrames.length; i++) {
+      this.destroyImage(this.character.rightFrames[i]);
+    }
+    for (let i = 0; i < this.character.leftFrames.length; i++) {
+      this.destroyImage(this.character.leftFrames[i]);
+    }
+    for (let i = 0; i < this.character.jumpFrames.length; i++) {
+      this.destroyImage(this.character.jumpFrames[i]);
+    }
+    for (let i = 0; i < this.character.shotFrames.length; i++) {
+      this.destroyImage(this.character.shotFrames[i]);
+    }
+    // 清空数组
+    this.character.rightFrames = [];
+    this.character.leftFrames = [];
+    this.character.jumpFrames = [];
+    this.character.shotFrames = [];
+  }
+  // 销毁图片时调用
+  destroyImage(imageObject) {
+    imageObject.src = '';
   }
 }
